aeb44c54d27d2659afe39379b7509aa46142c527
Cesar Kamide <ckamide@gmail.com>
2014-08-21 13:23:14 -0300

    teste

---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope) {
+})
+
+.controller('AccountCtrl', function($scope) {
+})
+
+.controller('ConfigCtrl', function($scope) {
+})
